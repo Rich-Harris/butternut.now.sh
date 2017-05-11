339e099333219f99bcca69915ebe5a5c8d9c1685
Rich-Harris <richard.a.harris@gmail.com>
2017-05-11 13:09:28 -0400

    use minified version of butternut where possible

--- worker/src/main.js
+++ worker/src/main.js
@@ -1,45 +1,62 @@
-// load Butternut
-const versionMatch = typeof window !== 'undefined'
-	? /version=([^&]+)/.exec(window.location.search)
-	: null;
-const version = versionMatch ? versionMatch[1] : 'latest';
-const url = version === 'local'
-	? `/butternut.umd.js`
-	: `https://unpkg.com/butternut@${version}/dist/butternut.umd.js`;
-
 let pendingSource;
 let butternut;
 
-const xhr = new XMLHttpRequest();
-xhr.onload = () => {
-	const fn = new Function(xhr.responseText);
-	const global = {};
+function init(search) {
+	// load Butternut
+	const versionMatch = /version=([^&]?(\d+)(?:\.(\d+))?(?:\.(\d+))?)/.exec(
+		search
+	);
+	const version = versionMatch ? versionMatch[1] : 'latest';
+	console.log('version', version);
 
-	fn.call(global);
-	butternut = global.butternut;
+	// 0.3.4 and newer get the minified version
+	const format = versionMatch
+		? +versionMatch[2] > 0 ||
+				!versionMatch[3] ||
+				(+versionMatch[3] > 3 ||
+					(+versionMatch[3] === 3 && !versionMatch[4])) ||
+				+versionMatch[4] >= 4
+				? 'min'
+				: 'umd'
+		: 'min';
 
-	console.log('ready');
+	const url = version === 'local'
+		? `/butternut.umd.js`
+		: `https://unpkg.com/butternut@${version}/dist/butternut.${format}.js`;
 
-	self.postMessage({
-		type: 'init',
-		version: version === 'local' ? version : global.butternut.VERSION
-	});
+	const xhr = new XMLHttpRequest();
+	xhr.onload = () => {
+		const fn = new Function(xhr.responseText);
+		const global = {};
 
-	if (pendingSource) {
-		squash(pendingSource);
-	}
-};
-xhr.onerror = event => {
-	self.postMessage({
-		type: 'error',
-		message: event.message
-	});
-};
-xhr.open('GET', url);
-xhr.send();
+		fn.call(global);
+		butternut = global.butternut;
+
+		console.log('ready');
+
+		self.postMessage({
+			type: 'init',
+			version: version === 'local' ? version : global.butternut.VERSION
+		});
+
+		if (pendingSource) {
+			squash(pendingSource);
+		}
+	};
+	xhr.onerror = event => {
+		self.postMessage({
+			type: 'error',
+			message: event.message
+		});
+	};
+	xhr.open('GET', url);
+	xhr.send();
+}
 
 self.addEventListener('message', event => {
 	switch (event.data.type) {
+		case 'init':
+			init(event.data.search);
 		case 'source':
 			if (butternut) {
 				squash(event.data.source);
@@ -52,7 +69,7 @@ self.addEventListener('message', event => {
 
 function squash(source) {
 	try {
-		const {code} = butternut.squash(source, {
+		const { code } = butternut.squash(source, {
 			check: true
 		});
 
